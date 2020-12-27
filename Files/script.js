86112a37a0cc46fa36f29076daa125bf97e2d710
kz-stmp <martinhom576@gmail.com>
2020-12-27 17:22:04 +0100

    adicionadas páginas de bolos e aprimorado o sistema de selecçao e validação de encomenda

--- Files/script.js
+++ Files/script.js
@@ -7,10 +7,9 @@ menuBtn.addEventListener('click', () => {
         menuOpen = true;
 
         const text = `<div class='receitas'>
-                        <h1>Receitas</h1>
                         <h2 class='mb-n3'>DOCES</h2>
                         <div class="d-flex justify-content-center mt-0"><hr class="l1"></div>
-                        <p><a href="#">Bolos</a></p>
+                        <p><a href="bolos.html">Bolos</a></p>
                         <p><a href="#">Cupcakes</a></p>
                         <p><a href="#">Tortas</a></p>
                         <p><a href="#">Pudim</a></p>
@@ -21,6 +20,12 @@ menuBtn.addEventListener('click', () => {
                         <p><a href="#">Rissóis</a></p>
                         <p><a href="#">Cochinhas</a></p>
                         <p><a href="#">Croquettes</a></p>
+
+                        <h2 class='mb-n3'>SMOOTHIES</h2>
+                        <div class="d-flex justify-content-center mt-0"><hr class="l1"></div>
+                        <p><a href="#">Smoothies com leite</a></p>
+                        <p><a href="#">Smoothies com iogurte</a></p>
+                        <p><a href="#">Smoothies sem leite ou iogurte</a></p>
                       </div>
                      `
 
@@ -32,4 +37,28 @@ menuBtn.addEventListener('click', () => {
 
         receitas.innerHTML = '';
     }
+});
+
+const opt1 = document.getElementById('opt1');
+const opt2 = document.getElementById('opt2');
+const opt3 = document.getElementById('opt3');
+const opt4 = document.getElementById('opt4');
+const opt5 = document.getElementById('opt5');
+
+var price = document.getElementById('preço');
+
+opt1.addEventListener('click', () => {
+    price.innerHTML= '200DA';
+});
+opt2.addEventListener('click', () => {
+    price.innerHTML= '500DA';
+});
+opt3.addEventListener('click', () => {
+    price.innerHTML= '700DA';
+});
+opt4.addEventListener('click', () => {
+    price.innerHTML= '800DA';
+});
+opt5.addEventListener('click', () => {
+    price.innerHTML= '1000DA';
 });
